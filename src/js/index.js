299e52c28a714f993ed0471134a3c5e15d2580ba
Danilo Santos <danilosantos14a@gmail.com>
2023-05-16 17:20:21 -0300

    Arquivo index alterado

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const botaoAlterTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro")
+    body.classList.toggle("modo-escuro");
+    if (modoEscuroEstaAtivo) {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+    } else {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+    }
+});
